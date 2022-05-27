1ceeb3cd5da79aa9b46957d465b84eb4eba1da6f
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2022-05-27 14:43:37 +0100

    Basic bot hello function

--- slackbot.py
+++ slackbot.py
@@ -16,5 +16,11 @@ SLACK_BOT_TOKEN = str(conf.get('ACCESS', 'SLACK_BOT_TOKEN'))
 # Initializes your app with your bot token and socket mode handler
 app = App(token=SLACK_BOT_TOKEN)
 
+# Listens to incoming messages that contain "hello"
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>! :partyparrot:")
+
 if __name__ == "__main__":
     SocketModeHandler(app, SLACK_APP_TOKEN).start()
