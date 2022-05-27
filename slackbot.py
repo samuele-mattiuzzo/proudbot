e4b86d8946abeda3ca32b0fcf4748807632fc069
Samuele Mattiuzzo <samuele.mattiuzzo@madetech.com>
2022-05-27 14:35:44 +0100

    First test connecting to slack using the tokens

--- slackbot.py
+++ slackbot.py
@@ -1,5 +1,10 @@
 import configparser
+import os
 
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Bot and App tokens
 SLACK_BOT_TOKEN = None
 SLACK_APP_TOKEN = None
 
@@ -8,6 +13,8 @@ conf.read('config.ini')
 SLACK_APP_TOKEN = str(conf.get('ACCESS', 'SLACK_APP_TOKEN'))
 SLACK_BOT_TOKEN = str(conf.get('ACCESS', 'SLACK_BOT_TOKEN'))
 
+# Initializes your app with your bot token and socket mode handler
+app = App(token=SLACK_BOT_TOKEN)
+
 if __name__ == "__main__":
-    print(SLACK_APP_TOKEN)
-    print(SLACK_BOT_TOKEN)
+    SocketModeHandler(app, SLACK_APP_TOKEN).start()
