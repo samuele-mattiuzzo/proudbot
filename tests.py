1bcd67df112b74c4d2b68b3b7e418f2cbd92e546
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-06-24 15:08:13 +0100

    Add a basic unit test for create_person_id()

--- tests.py
+++ tests.py
@@ -3,5 +3,8 @@ from helpers import create_person_id
 
 class TestHelperMethods(unittest.TestCase):
 
+    def test_create_person_id(self):
+        self.assertEqual(create_person_id("Angelica Ross"), "angelica_ross")
+
 if __name__ == '__main__':
     unittest.main()
