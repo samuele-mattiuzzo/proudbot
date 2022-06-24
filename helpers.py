70fdaec527e63ef8b8a36d4043e50786e5b6f67d
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-06-24 14:48:22 +0100

    Remove empty line

--- helpers.py
+++ helpers.py
@@ -2,7 +2,6 @@
 
 MOCK_PRIDE_FACTS_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-
 def create_person_id(name):
     return name.lower().replace(" ", "_")
 
