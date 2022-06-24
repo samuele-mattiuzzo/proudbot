cfe4bc4985ce282f3d555d64b1da9af3e0e70fcd
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-06-24 15:16:03 +0100

    Add basic unit test for text_matches()

--- tests.py
+++ tests.py
@@ -1,10 +1,17 @@
 import unittest
-from helpers import create_person_id
+from helpers import create_person_id, text_matches
 
 class TestHelperMethods(unittest.TestCase):
 
     def test_create_person_id(self):
         self.assertEqual(create_person_id("Angelica Ross"), "angelica_ross")
+
+    def test_text_matches(self):
+        match_phrase = "hello"
+        self.assertTrue(text_matches("hello", match_phrase))
+        self.assertTrue(text_matches("Hello", match_phrase))
+        self.assertTrue(text_matches("hello!", match_phrase))
+        self.assertTrue(text_matches("HelLo!", match_phrase))
 
 if __name__ == '__main__':
     unittest.main()
