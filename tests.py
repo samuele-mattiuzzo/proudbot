ad001c464d53c2b22680fd7ecf765e9d8a50981f
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-06-29 16:39:21 +0100

    Add basic test to be able to work with the request

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from helpers import create_person_id, text_matches
+from helpers import create_person_id, text_matches, get_quote
 
 class TestHelperMethods(unittest.TestCase):
 
@@ -12,6 +12,9 @@ class TestHelperMethods(unittest.TestCase):
         self.assertTrue(text_matches("Hello", match_phrase))
         self.assertTrue(text_matches("hello!", match_phrase))
         self.assertTrue(text_matches("HelLo!", match_phrase))
+
+    def test_get_guotes(self):
+        self.assertEqual(get_quote(), 200)
 
 if __name__ == '__main__':
     unittest.main()
