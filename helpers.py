c399e258e8c17dcebea06271c3bfc9bf3f8d1e89
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-07-01 12:30:17 +0100

    Add helper functions to generate a random quote

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 # Helper functions
 
 import re
+import json
+import random
 
 MOCK_PRIDE_FACTS_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
@@ -30,3 +32,17 @@ def read_file(person_id):
 
 def text_matches(text, match_phrase):
     return bool(re.search(match_phrase, text, re.IGNORECASE))
+
+def read_json_file(file):
+    f = open(file)
+    data = json.load(f)
+    print(data)
+    f.close()
+    return data
+
+def get_random_quote():
+    all_quotes = read_json_file("quotes.json")
+    random_quote_id = random.randrange(0, len(all_quotes) - 1)
+    print(random_quote_id)
+    random_quote = all_quotes[random_quote_id]
+    return random_quote
