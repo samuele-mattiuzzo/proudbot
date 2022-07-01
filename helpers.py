5925be0d1a5fe0e4cf761edf1b720f2bc971f23c
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2022-07-01 14:25:22 +0100

    Replace MOCK_PRIDE_FACTS_TEXT with "No information found" message

--- helpers.py
+++ helpers.py
@@ -4,8 +4,6 @@ import re
 import json
 import random
 
-MOCK_PRIDE_FACTS_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
 def create_person_id(name):
     return name.lower().replace(" ", "_")
 
@@ -28,7 +26,7 @@ def read_md_file(person_id):
         with open('peoplez/' + person_id + '.md', 'r') as f:
             return f.read()
     except FileNotFoundError:
-        return MOCK_PRIDE_FACTS_TEXT
+        return "No information found"
 
 def text_matches(text, match_phrase):
     return bool(re.search(match_phrase, text, re.IGNORECASE))
